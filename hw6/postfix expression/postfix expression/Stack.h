a9d169808d2b3caf64998e11b403c3c667da14c8
Victoria <viky.fomina@yandex.ru>
2018-10-27 02:16:38 +0300

    added stack

---
+++ hw6/postfix expression/postfix expression/Stack.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+struct Element;
+
+struct Stack
+{
+	// конструктор
+	Stack();
+	// деструктор
+	~Stack();
+	// конструктор копирования
+
+	// оператор присваивания
+
+	// добавление элемента в голову
+	void push(char data);
+	// удаление элемента из головы
+	int pop();
+	// посмотреть на головной элемент
+	char peek();
+	// проверяет является ли стек пустым
+	bool isEmpty();
+private:
+	Element * head;
+};
