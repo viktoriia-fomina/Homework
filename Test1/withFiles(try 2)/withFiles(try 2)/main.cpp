86f2dac2c57de4a44153316b085ec0681267985b
Victoria <viky.fomina@yandex.ru>
2018-10-26 17:38:07 +0300

    seems ready

---
+++ Test1/withFiles(try 2)/withFiles(try 2)/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+// В некоторых языках программирования однострочные комментарии задаются не //, 
+// как в С++, а символом ";" (комментарий начинается с ; и заканчивается концом строки). 
+// Задача - вывести на консоль все комментарии такого вида из входного файла (вместе с символом ";"). 
+// До комментария в строке может быть значимый текст, его выводить не надо. 
+// Пустые строки выводить не надо.
+
+int main()
+{
+	ifstream file("test.txt", ios::in);
+	if (!file.is_open())
+	{
+		cout << "File not found" << endl;
+		return 1;
+	}
+	else
+	{
+		int const size = 100;
+		char buffer[size];
+		bool flagIfSymbol = false;
+		while (!file.eof())
+		{
+			file.getline(buffer, size);
+			int length = strlen(buffer);
+			for (int i = 0; i < length; ++i)
+			{
+				if (flagIfSymbol)
+				{
+					cout << buffer[i];
+				}
+				if (buffer[i] == ';')
+				{
+					cout << ";";
+					flagIfSymbol = true;
+				}
+			}
+			if (flagIfSymbol)
+			{
+				cout << endl;
+			}
+			flagIfSymbol = false;
+		}
+	}
+
+
+	system("pause");
+	return 0;
+}
