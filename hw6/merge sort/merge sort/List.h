f8e88d3b5f5fccefa23c2a26853e4b13a9fa20fd
Victoria <viky.fomina@yandex.ru>
2018-10-29 13:44:58 +0300

    some changes

--- hw6/merge sort/merge sort/List.h
+++ hw6/merge sort/merge sort/List.h
@@ -8,15 +8,14 @@ struct Node;
 
 struct List
 {
-	// êîíñòðóêòîðû - äåñòðóêòîðû, äîáàâëåíèå - óäàëåíèå
 	List();
 	~List();
-	void addNode(int const data); // 1
-	void deleteNode(int const data); // 2
+	void addNode(int const data);
+	void deleteNode(int const data);
 									 // îñòàëüíîå
+	bool isEmpty() const;
 	friend ostream & operator<<(ostream & os, List const & list); // 3
 private:
-	bool isEmpty() const;
 	void deleteHead(int const data, Node* temp);
 	void deleteElementThatIsNotHead(int const data, Node* temp);
 	void swap(Node * a, Node* b);
