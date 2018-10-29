db69ee5264df5814f6541353eabcdb455d6c6a6b
Victoria <viky.fomina@yandex.ru>
2018-10-29 13:46:36 +0300

    small change

--- hw5/List/List/List.h
+++ hw5/List/List/List.h
@@ -15,8 +15,8 @@ struct List
 	void deleteNode(int const data); // 2
 	// остальное
 	friend ostream & operator<<(ostream & os, List const & list); // 3
-private:
 	bool isEmpty() const;
+private:
 	void deleteHead(int const data, Node* temp);
 	void deleteElementThatIsNotHead(int const data, Node* temp);
 	void swap(Node * a, Node* b);
