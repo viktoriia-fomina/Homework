cd4bff2d81324e856f880b6e0723f25611380098
Victoria <viky.fomina@yandex.ru>
2018-11-03 17:31:12 +0300

    some changes

---
+++ hw7/ADT Set (Tree)/ADT Set (Tree)/List.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+struct Element;
+
+struct List
+{
+	List();
+	~List();
+	void addNode(int const data);
+	void deleteNode(int const data);
+	void printInAscendingOrder();
+	void printInDescendingOrder();
+	bool isEmpty() const;
+private:
+	void deleteHead(int const data, Element* temp);
+	void deleteElementThatIsNotHead(int const data, Element* temp);
+	void swap(Element * a, Element* b);
+	Element* head;
+};
