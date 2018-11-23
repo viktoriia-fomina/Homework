02b726bdcd5424b22e57a7eed2616294b7c9ba91
Victoria <viky.fomina@yandex.ru>
2018-11-24 01:38:06 +0300

    added tests

--- hw6/merge sort/merge sort/main.cpp
+++ hw6/merge sort/merge sort/main.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
-#include "List.h"
-#include <string>
-#include <fstream>
-#include "Node.h"
+#include "userInterface.h"
+#include "Tests.h"
 
 using namespace std;
 
 int main()
 {
-	List l;
-	l.addNode("aaa");
-	l.addNode("lala");
-	l.addNode("caca");
-	l.addNode("trolo");
-	l.addNode("epic");
-	/*l.addNode("1323");
-	l.addNode("90073");
-	l.addNode("132");
-	l.addNode("8203");*/
-	cout << l << endl;
-	cout << l.size() << endl;
-	l.mergeSort();
-	cout << l << endl;
-
-
+	tests();
+	userInterface();
 
 	return 0;
 }
