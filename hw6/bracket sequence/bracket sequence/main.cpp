52d86bf7d48356ab2be51beaec3589b51e4109d5
Victoria <viky.fomina@yandex.ru>
2018-12-18 16:59:52 +0300

    added tests

--- hw6/bracket sequence/bracket sequence/main.cpp
+++ hw6/bracket sequence/bracket sequence/main.cpp
@@ -2,6 +2,7 @@
 #include "bracket sequence.h"
 #include <string>
 #include <iostream>
+#include "tests.h"
 
 // Написать программу проверки баланса скобок в строке, скобки могут быть трёх видов 
 // : (), [], {}.Скобочная последовательность вида({ ) } считается некорректной, ({}) - корректной.
@@ -10,6 +11,7 @@ using namespace std;
 
 int main()
 {
+	tests();
 	string s;
 	cout << "Input string" << endl;
 	cin >> s;
