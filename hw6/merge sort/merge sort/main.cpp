b35e6f8d4199c6b7756169e45903f8e592954f32
Victoria <viky.fomina@yandex.ru>
2018-12-20 16:26:50 +0300

    nothing was changed

--- hw6/merge sort/merge sort/main.cpp
+++ hw6/merge sort/merge sort/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-#include "userInterface.h"
-#include "Tests.h"
+#include "List.h"
+#include <string>
+#include <fstream>
+#include "Node.h"
 
 using namespace std;
 
 int main()
 {
-	tests();
-	userInterface();
+	List l;
+	l.addNode("aaa");
+	l.addNode("lala");
+	l.addNode("caca");
+	l.addNode("trolo");
+	l.addNode("epic");
+	/*l.addNode("1323");
+	l.addNode("90073");
+	l.addNode("132");
+	l.addNode("8203");*/
+	cout << l << endl;
+	cout << l.size() << endl;
+	l.mergeSort();
+	cout << l << endl;
+
+
 
 	return 0;
 }
