a5fdece46523dd2c1ab33c1238ba3d8fcc1a1a62
Victoria <viky.fomina@yandex.ru>
2018-12-21 16:41:05 +0300

    фыва

--- LastTest/Task3/Task3/forMatrix.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void incidenceMatrixToAdjencyMatrix(int numberOfVertexes, int numberOfEdges,
-		int** incidenceMatrix, int** adjencyMatrix);
-
-void reachableVertexes(int numberberOfVertexes, int** adjencyMatrix);
