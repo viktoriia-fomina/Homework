27ff3cee9d83fadbba93a97a554d0ee81c276f49
Victoria <viky.fomina@yandex.ru>
2018-12-22 13:24:39 +0300

    renamed without spaced and added Makefile

---
+++ hw6/toPostfix/toPostfix/toPostfix/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "toPostfix.h"
+
+using namespace std;
+
+int main()
+{
+	string infix;
+	cout << "Input expression to convert it to postfix\n";
+	cin >> infix;
+	string postfix;
+	toPostfix(infix, postfix);
+	cout << "Result is\n";
+	cout << postfix << endl;
+
+	system("pause");
+	return 0;
+}
