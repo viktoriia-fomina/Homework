1203bd073f9e3ee6ae46bdcfbbe9a0462c39ca14
Victoria <viky.fomina@yandex.ru>
2018-12-26 13:28:03 +0300

    main algorithm

--- hw10/Graph/Graph/countries.cpp
+++ hw10/Graph/Graph/countries.cpp
@@ -21,4 +21,25 @@ void addCityToCountry(set<int> & country, set<int> & unvisitedCities, Graph cons
 
 void printCountries(vector<set<int>> const & countries);
 
-vector<set<int>> countriesSeparator(string const filename);
+vector<set<int>> countriesSeparator(string const filename)
+{
+	//создаём пустые контейнеры для данных
+	Graph graph;
+	vector<set<int>> countries;
+	set<int> unvisitedCities;
+
+	//инициализация данных
+	bool fileIsNotOpen = getDataFromFile(graph, countries, unvisitedCities, filename);
+	if (!fileIsNotOpen)
+	{
+		cout << "File is not open!!!";
+		return countries;
+	}
+
+	// основной алгоритм, добавление городов в страны
+	countriesDivider(graph, countries, unvisitedCities);
+
+	// вывод результата
+	printCountries(countries);
+	return countries;
+}
