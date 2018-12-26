5b6b6320ed951afac22a63a9f76a4f29dff9de0b
Victoria <viky.fomina@yandex.ru>
2018-12-26 17:16:03 +0300

    get data from file

--- hw10/Graph/Graph/countries.cpp
+++ hw10/Graph/Graph/countries.cpp
@@ -43,3 +43,43 @@ vector<set<int>> countriesSeparator(string const filename)
 	printCountries(countries);
 	return countries;
 }
+
+bool getDataFromFile(Graph & graph, vector<set<int>> & countries,
+	set<int> & unvisitedCities, string const filename)
+{
+	ifstream file(filename);
+	if (!file.is_open())
+	{
+		return false;
+	}
+
+	// ввод городов и расстояний между ними
+	int numberOfCities;
+	file >> numberOfCities;
+	assert(numberOfCities > 0);
+	graph.resize(numberOfCities);
+	int numberOfEdges;
+	file >> numberOfEdges;
+	for (int edge = 0; edge < numberOfEdges; ++edge)
+	{
+		int row;
+		file >> row;
+		int column;
+		file >> column;
+		file >> graph.edgeWeight(row, column);
+		graph.edgeWeight(column, row) = graph.edgeWeight(row, column);
+	}
+	graph.symmetricAndNoLoopsAdjacencyMatrix();
+
+	// ввод количества стран и их столиц, инициализация стран и непосещённых городов
+	int numberOfCountries;
+	file >> numberOfCountries;
+	assert(numberOfCountries > 0 && numberOfCountries <= numberOfCities);
+	countries.resize(numberOfCountries);
+	unvisitedCities = graph.getSetOfVertexes();
+
+	// пихаем столицы в страны и удаляем столицы из непосещённых
+	initializeCountriesFromFile(countries, unvisitedCities, file);
+	file.close();
+	return true;
+}
